8cec166d96890a9cbd469c91ae1270a29082eaf3
roblox39008 <maximcherepanoff@gmail.com>
2026-05-19 18:59:25 +0400

    Add files via upload

---
+++ 10/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            double c = double.Parse(Console.ReadLine());
+            double max = Math.Max(a, Math.Max(b, c));
+            double min = Math.Min(a, Math.Min(b, c));
+            Console.WriteLine($"Наибольшее: {max}");
+            Console.WriteLine($"Наименьшее: {min}");
+            Console.WriteLine("\nНажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace _11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            double c = double.Parse(Console.ReadLine());
+            Console.Write("Введите четвёртое число: ");
+            double d = double.Parse(Console.ReadLine());
+            double max = Math.Max(a, Math.Max(b, Math.Max(c, d)));
+            double min = Math.Min(a, Math.Min(b, Math.Min(c, d)));
+            Console.WriteLine($"Наибольшее: {max}");
+            Console.WriteLine($"Наименьшее: {min}");
+            Console.WriteLine("\nНажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
